a86711396cf1404437f0daaad0f914f5f3f22a0c
Shreyansh <aayansrivastava1912@gmail.com>
2022-03-19 23:00:53 +0530

    March LunchTime

---
+++ src/com/company/CodeChef17.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+class CodeChef17{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        if(X>=750){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
