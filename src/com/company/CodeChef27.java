d948e789cc754e4b25df3307f854122893bc6bde
Shreyansh <aayansrivastava1912@gmail.com>
2022-03-26 23:25:57 +0530

    The Rating Dilemma

---
+++ src/com/company/CodeChef27.java
@@ -0,0 +1,24 @@
+package com.company;
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class CodeChef27
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int s = sc.nextInt();
+
+            int x=1;
+            int y=-x-s;
+            System.out.println(y);
+        }
+    }
+}
+
