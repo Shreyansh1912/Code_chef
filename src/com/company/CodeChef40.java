a8c56413267f3adb3a7d3b2f407a4211c7d82fa5
Shreyansh <aayansrivastava1912@gmail.com>
2022-04-08 22:46:44 +0530

    The Cooler Dilemma 1

---
+++ src/com/company/CodeChef40.java
@@ -0,0 +1,25 @@
+package com.company;
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CodeChef40{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int m=sc.nextInt();
+            if(x*m < y && x*m != y) System.out.println("YES");
+            else System.out.println("NO");
+
+        }
+    }
+}
+
