0fc938f5b0719914adf392620df65351aab73f80
Shreyansh <aayansrivastava1912@gmail.com>
2022-04-08 22:43:41 +0530

    Ezio and Guards

---
+++ src/com/company/CodeChef39.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class CodeChef39
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            if(x>=y) System.out.println("yes");
+            else System.out.println("no");
+
+
+        }
+    }
+}
+
