dc9d79ee122aae8cd0c2dec3eb5cd05804c6ee92
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-06-05 23:05:50 +0530

    Rating Improvement (June Cook-0ff)

---
+++ src/com/company/CodeChef60.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CodeChef60
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    if(y >= x && y <= x+200)
+		    {
+		        System.out.println("YES");
+		    }
+		    else{
+		         System.out.println("NO");
+		    }
+		}
+	}
+}
