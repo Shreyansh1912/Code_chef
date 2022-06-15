dab7d121eda846c65d99fe36026e945cc9871539
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-06-15 23:10:33 +0530

    Waiting Time(Starters 43 )

---
+++ src/com/company/CodeChef66.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int k = sc.nextInt();
+		    int x = sc.nextInt();
+		    System.out.println((k*7)-x);
+		}
+	}
+}
