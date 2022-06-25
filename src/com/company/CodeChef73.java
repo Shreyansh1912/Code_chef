1e29470ad75a2b1f15cded157a7d05cae9970237
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-06-25 17:53:31 +0530

    Fitness(June Long)

---
+++ src/com/company/CodeChef73.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    x = 2*x;
+		    System.out.println(x*5);
+		}
+	}
+}
