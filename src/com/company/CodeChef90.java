e913ebb3cb60abc6bc324dfe1f4d60262b9a5d8a
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-09-14 20:02:43 +0530

    Broken Phone

---
+++ src/com/company/CodeChef90.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    	int a = sc.nextInt();
+		    	int b = sc.nextInt();
+		    	
+		    	if(a == b) System.out.println("ANY");
+		    	else if(a > b) System.out.println("NEW PHONE");
+		    	else System.out.println("REPAIR");
+		}
+	}
+}
