4bf71633e0427397eeef966046b93d212462bb24
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-09-17 23:36:49 +0530

    Chef and Wire Frames

---
+++ src/com/company/CodeChef92.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner sc = new Scanner(System.in);
+	   int t = sc.nextInt();
+	   while(t-->0){
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+	        int c = sc.nextInt();
+	        
+	        int d = 2*a + 2*b;
+	        
+	        System.out.println(d*c);
+	   }
+	}
+}
