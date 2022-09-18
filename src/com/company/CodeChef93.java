fc96957963dd0bc22629fa8266666eeeba5c8f6f
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-09-18 22:23:31 +0530

    NEAREST EXIT

---
+++ src/com/company/CodeChef93.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      Scanner sc = new Scanner(System.in);
+      int x = sc.nextInt();
+      while(x-->0){
+            int t = sc.nextInt();
+            if(t<=50) System.out.println("LEFT");
+            else System.out.println("RIGHT");
+      }
+	}
+}
