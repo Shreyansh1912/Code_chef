b240311faba51d5afbfe32f0ea7f1791b8730435
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-10-26 22:21:52 +0530

    Make Multiple

---
+++ src/com/company/CodeChef109.java
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+		int t;
+		cin>>t;
+		while(t--){
+	     	int a,b;
+	     	cin>>a>>b;
+		
+		if(b%a){
+		    if(b>2*a)
+		        cout << "YES" << endl;
+		    else
+		       cout << "NO" << endl;
+		} else{
+		    cout << "YES" << endl;
+		  }
+		}
+	return 0;
+}
