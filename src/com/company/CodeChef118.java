53fe54ac1a8d682b3e73e6f81865858dfe9e2755
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-11-16 20:47:02 +0530

    Parity(Starter 65)

---
+++ src/com/company/CodeChef118.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    
+		    if(x % 2 == 0){
+		        System.out.println("Yes");
+		    }else{
+		        System.out.println("No");
+		    }
+		}
+	}
+}
