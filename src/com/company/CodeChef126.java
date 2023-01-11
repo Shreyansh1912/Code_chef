13541fbfb9a997a712dbff636404a23b20755b92
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2023-01-11 20:53:56 +0530

    Chef Plays Ludo (Starter 73)

---
+++ src/com/company/CodeChef126.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    if(x == 6)System.out.println("Yes");
+		    else System.out.println("No");
+		}
+	}
+}
