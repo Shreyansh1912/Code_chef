09a78841303ea19b35377312e6ff6791aa41da11
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2023-06-14 22:32:19 +0530

    Gymkhana Election IIIT-A.java

---
+++ src/com/company/CodeChef135.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    
+		    int c = (b/2)+1;
+		    System.out.println(c);
+		 }
+	}
+}
